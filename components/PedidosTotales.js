121220e54e946432a06f848c78d98c1f3ca29429
EmilyCruz01 <96510519+EmilyCruz01@users.noreply.github.com>
2023-11-22 20:44:20 -0600

    Update PedidosTotales.js

--- components/PedidosTotales.js
+++ components/PedidosTotales.js
@@ -1,8 +1,26 @@
+"use client"
+import { useState } from "react";
 import "/styles/pedidosTotales.css"
 import Link from "next/link";
 
 function PedidosTotalesC(){
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
+
+    const [opcionSeleccionada, setOpcionSeleccionada] = useState('');
+
+    const handleSeleccion = (event) => {
+      setOpcionSeleccionada(event.target.value);
+    };
     return(
+        
         <section>
             <Link href="/pedidosTotales/pedidosOpc"><div id="pLlevar">
                 <h1>Para llevar 1</h1>
@@ -14,9 +32,34 @@ function PedidosTotalesC(){
 
             </div>
 
-            <Link href="/pedidosTotales/pedidosOpc"><button id="btnAgregar">Agregar</button></Link>
+            <button onClick={openModal} id="btnAgregar">Agregar</button>
+
+            {showModal && (
+                <div className="modalAtras">
+                    <div className="modalAdentro"> 
+                         {/* Ingresar la foto o los datos de lo que esta en el delete logico*/}
+                         <div id="opcionesPedido">
+                    
+                            <select id="mesasOpc" value={opcionSeleccionada} onChange={handleSeleccion}>
+                                <option value="">Mesas</option>
+                                <option value="opcion1">Mesa 1</option>
+                                <option value="opcion2">Mesa 2</option>
+                                <option value="opcion3">Mesa 3</option>
+                            </select>
+
+                            <button id="btnPLlevar">Para llevar</button>
+                         </div>
+                         <div className="o">
+                            <Link href="/inventario/agregarProducto"><button id="btn-Confirmar" onClick={closeModal}>Confirmar</button></Link>
+                            <button id="btn-Cerrar" onClick={closeModal}>Cancelar</button>
+                         </div>
+                       </div>
+                    
+                </div>
+            )}
         </section>
-    )
+
+    );
 }
 
 export default PedidosTotalesC;
